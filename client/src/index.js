97f8a193c86fdd5811331836cc9e3b34831644c4
dennis <kipkiruidennis25@gmail.com>
2024-10-16 08:51:47 +0300

    index

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,13 @@
-// index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' for React 18
-import App from './components/App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css'; // Importing global styles
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from './Routes';
+import './index.css';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
