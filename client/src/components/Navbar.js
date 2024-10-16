b4057b4f50505bfe19c15cd925f0b0d9da627249
dennis <kipkiruidennis25@gmail.com>
2024-10-16 08:52:53 +0300

    nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,24 @@
-// Navbar.js
-import React, { useState } from 'react';
+// src/components/Navbar.js
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const [isHovering, setIsHovering] = useState(false);
+  const navItems = [
+    { name: 'Home', path: '/' },
+    { name: 'Sign Up', path: '/signup' },
+    { name: 'Patient Dashboard', path: '/patient-dashboard' },
+  ];
 
   return (
-    <nav>
-      <ul>
-        <li 
-          onMouseEnter={() => setIsHovering(true)} 
-          onMouseLeave={() => setIsHovering(false)}
-        >
-          <Link to="/patient-dashboard">Patient Dashboard</Link>
-          {isHovering && (
-            <ul className="dropdown">
-              <li><Link to="/patient-dashboard/details">Patient Details</Link></li>
-              <li><Link to="/patient-dashboard/departments">Departments</Link></li>
-              <li><Link to="/patient-dashboard/book-appointment">Book Appointment</Link></li>
-            </ul>
-          )}
-        </li>
+    <nav className="navbar">
+      <ul className="nav-list">
+        {navItems.map((item, index) => (
+          <li key={index} className="nav-item">
+            <Link className="nav-link" to={item.path}>
+              {item.name}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
