5d2a6b2ea82f5a38e1f8eadfca1d652818d94b29
Samuel Ndegwa <samuel.ndegwa@student.strathmore.edu>
2024-10-16 08:28:12 -0400

    update header js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import About from './About';
+import Home from './Home';
+import Contact from './Contact';  // Import Contact component
 
+function App() {
   return (
-    <>
-    <div className="App">
-      <About />{About}
-    </div>
-    </>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | {" "}
+        <Link to="/about">About</Link> | {" "}
+        <Link to="/contact">Contact</Link> {/* Add Contact link */}
+      </nav>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />     {/* Home route */}
+          <Route path="/about" element={<About />} /> {/* About route */}
+          <Route path="/contact" element={<Contact />} /> {/* Contact route */}
+        </Routes>
+      </div>
+    </Router>
   );
-
+}
 
 export default App;
